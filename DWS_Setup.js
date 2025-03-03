f885e3c42fec4bfccf8d6fcbd4d5157838fe13f9
marklula <marklula+github@gmail.com>
2025-03-02 21:42:15 -0500

    Update DWS_Setup.js

--- DWS_Setup.js
+++ DWS_Setup.js
@@ -159,11 +159,11 @@ async function firstSetup()
   // ????? NEEDED ?????
 
   // DELETE SETUP MACROS AND ENABLE CORE MACRO
+  xapi.Command.UserInterface.Extensions.Panel.Remove({ PanelId: 'dws_wizard_start' });
   xapi.Command.UserInterface.Extensions.Panel.Remove({ PanelId: 'dws_wizard_confirm' });
   try { xapi.Command.Macros.Macro.Activate({ Name: 'DWS_Core' }); } catch(error) { console.error('DWS: Error Starting Core Macro: ' + error.message); }
-  try { xapi.Command.Macros.Macro.Deactivate({ Name: "DWS_Wizard" }); } catch(error) { console.error('DWS: Error Disabling Wizard Macro: ' + error.message); }
-  try { xapi.Command.Macros.Macro.Deactivate({ Name: "DWS_Setup" }); } catch(error) { console.log('DWS: Error Disabling Setup Macro: ' + error.message); }
-  //xapi.Command.Macros.Macro.Remove({ Name: "DWS_Wizard" });  
+  try { xapi.Command.Macros.Macro.Remove({ Name: "DWS_Wizard" }); } catch(error) { console.error('DWS: Error Deleting Wizard Macro: ' + error.message); }
+  try { xapi.Command.Macros.Macro.Remove({ Name: "DWS_Setup" }); } catch(error) { console.log('DWS: Error Deleting Setup Macro: ' + error.message); }
   setTimeout(() => {
         xapi.Command.Macros.Runtime.Restart()
           .catch(error => console.log('DWS: Error restarting Macro Engine: ' + error.message));
