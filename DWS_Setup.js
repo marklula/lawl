ee8d2ce845c5011a0d414fba8899243ee3c94bd5
marklula <marklula+github@gmail.com>
2025-03-03 00:24:49 -0500

    Update DWS_Setup.js

--- DWS_Setup.js
+++ DWS_Setup.js
@@ -214,8 +214,8 @@ async function configureC1K() {
 
   // SEND THREE EMPTY STRINGS TO VALIDATE READINESS THEN LOGIN
   if (DWS.DEBUG == 'true') {console.debug("DWS: Completing Initial Switch Setup via Serial")};
-  setTimeout (async () => { await sendSerialCommand('\\r'); }, 300);
-  setTimeout (async () => { await sendSerialCommand('\\r'); }, 300);
+  setTimeout (async () => { await sendSerialCommand(''); }, 300);
+  setTimeout (async () => { await sendSerialCommand(''); }, 300);
   await sendSerialCommand('cisco'); // DEFAULT USERNAME 
   await sendSerialCommand('cisco'); // DEFAULT PASSWORD
   await sendSerialCommand('dwsadmin');  // STANDARD USER FOR ONBOARDING
